3b2d85fa43afdd2e06190f2816963db5decfcf94
cheesefoo <cheesefoo666@gmail.com>
2021-08-05 18:57:49 -0400

    missing files

---
+++ src/routes/congrats-messages/index.js
@@ -0,0 +1,22 @@
+import faker from "faker";
+
+
+
+const generateLorem = () =>
+	[...Array(50)].map(() => {
+		const lorem = faker.lorem.paragraph()
+		const lastName = faker.name.lastName();
+
+		return {
+			avatar: `https://avatars.dicebear.com/api/human/${lastName}.svg`,
+			lastName,
+			lorem,
+
+		};
+	});
+
+export async function get() {
+	return {
+		body: generateLorem(),
+	};
+}
