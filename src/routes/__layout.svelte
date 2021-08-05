af564c4d72d96c8ca619660bce20f2e1d2fccc9b
cheesefoo <cheesefoo666@gmail.com>
2021-08-05 18:51:31 -0400

    take out l18n for now

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,16 +3,9 @@
 	import Panel from '$lib/components/Panel.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import '../app.scss';
-	import { initI18n } from '../i18n/i18n-svelte';
 
-	export async function load({ page, fetch, session, context })
-	{
-		// detect locale of user (see https://github.com/ivanhofer/typesafe-i18n#locale-detection)
-		const locale = 'en';
-		await initI18n(locale);
 
-		return {};
-	}
+
 </script>
 <main>
 	<Header />
