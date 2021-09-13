369e605a5e9fdeeffa2b8319bcefacb67c42c908
cheesefoo <cheesefoo666@gmail.com>
2021-09-12 20:34:48 -0400

    add more credits

--- src/lib/components/Panel.svelte
+++ src/lib/components/Panel.svelte
@@ -1,40 +1,56 @@
 <main>
-	<div class="inner-panel">
-		<div class="panel-content">
+	<div class='inner-panel'>
+		<div class='panel-content'>
 			<slot />
 		</div>
 	</div>
 </main>
 
-<style lang="scss">
-	main {
-		display: flex;
-		overflow: hidden;
-		align-content: center;
-		align-items: center;
-		flex-wrap: wrap;
-		justify-content: space-around;
-		width: 100%;
-		padding-top: 5vh;
-		padding-bottom: 5vh;
-		justify-items: center;
-	}
+<style lang='scss'>
+  main {
+    display: flex;
+    overflow: hidden;
+    align-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    width: 100%;
+    padding-top: 5vh;
+    padding-bottom: 5vh;
+    justify-items: center;
+  }
 
-	.inner-panel {
-		min-height: calc(50vh - 80px);
-		min-width: max(80vw, 500px);
-		background-color: $ivory;
-		display: block;
-		max-width: 50%;
-		margin: 0 0 0 8px;
-		vertical-align: middle;
-		border: 2px solid $chromeblue;
-		border-radius: 5px;
-		@include boxshadow($chromeblue);
-	}
+  .inner-panel {
+    min-height: calc(50vh - 80px);
+    min-width: max(80vw, 500px);
+    background-color: $ivory;
+    display: block;
+    max-width: 50%;
+    margin: 0 0 0 8px;
+    vertical-align: middle;
+    border: 2px solid $chromeblue;
+    border-radius: 5px;
+    @include boxshadow($chromeblue);
+  }
 
-	.panel-content {
-		padding: 8px;
-		margin: 8px;
-	}
+  .panel-content {
+    padding: 8px;
+    margin: 8px;
+  }
+
+  @media screen and (max-width: 849px) {
+    main {
+      padding: 1em 0;
+
+    }
+    .inner-panel {
+
+      min-width: max(92%, 350px);
+      max-width: 0;
+
+
+    }
+
+
+  }
 </style>
--- src/routes/projects/ponds.svelte
+++ src/routes/projects/ponds.svelte
@@ -1,8 +1,10 @@
 <script>
 	import Chart from 'chart.js/auto/auto.js';
 	import { onMount } from 'svelte';
+	import LL, {locale}from '/src/i18n/i18n-svelte';
+	import { media } from '$lib/stores/stores';
 
-	const labelsHairstyle = ['Short', 'Long', 'Twintails', "Can't decide"];
+	const labelsHairstyle = ['Short', 'Long', 'Twintails', 'Can\'t decide'];
 	const hairData = [1114, 483, 241, 49];
 	const labelsSubmissions = ['Fanart', 'Default Duck', 'Editor-made Duck', 'Uploaded Duck'];
 	const submissionData = [46, 250, 1680, 109];
@@ -116,6 +118,7 @@
 		options: {
 			responsive: true,
 			plugins: {
+
 				legend: {
 					position: 'top'
 				},
@@ -164,102 +167,171 @@
 			}
 		}
 	};
+if($media.small || $media.tablet){
+	configReferer.options.maintainAspectRatio=false;
+	configCountry.options.maintainAspectRatio=false;
 
+}
 	onMount(() => {
 		const hairstyle = new Chart(document.getElementById('hairstyle'), configHairstyle);
 		const submissions = new Chart(document.getElementById('submissions'), configSubmissions);
 		const loc = new Chart(document.getElementById('loc'), configCountry);
 		const referer = new Chart(document.getElementById('referer'), configReferer);
 	});
+	let yt = 'https://www.youtube.com/embed/YqRGwmk4fmw';
+	if ($locale == 'en') {
+		yt = 'https://www.youtube.com/embed/YqRGwmk4fmw';
+	} else {
+		yt = '';
+	}
 </script>
 
 <main>
-	<h1>Subatomo Ponds</h1>
-	<h3>Birthday & 1 Million Subscriber Celebration Project</h3>
-	<a href="https://ponds.subatomos.com">Visit the site here!</a>
-	<div class="video-container">
-		<iframe
-			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-			allowfullscreen
-			frameborder="0"
-			height="720"
-			src="https://www.youtube.com/embed/YqRGwmk4fmw"
-			title="YouTube video player"
-			width="1280"
-		/>
+	<h1>{$LL.HEADER.PONDS()}</h1>
+	<h3>{$LL.HOME.BIRTHDAY()}</h3>
+	<a href='https://ponds.subatomos.com'>Visit the site here!</a>
+	<div class='video-container'>
+		{#if $locale==='ja'}
+			<iframe
+				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+				allowfullscreen
+				frameborder='0'
+				height='720'
+				src='https://www.youtube.com/embed/pFD48nsdb_8'
+				title='YouTube video player'
+				width='1280'
+			/>
+			{:else}
+			<iframe
+				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+				allowfullscreen
+				frameborder='0'
+				height='720'
+				src='https://www.youtube.com/embed/YqRGwmk4fmw'
+				title='YouTube video player'
+				width='1280'
+			/>
+			{/if}
 	</div>
 	<p>We surveyed users during their submissions. These are the results:</p>
-	<div class="charts">
-		<canvas id="hairstyle" />
-		<canvas id="submissions" />
+	<div class='charts'>
+		<canvas id='hairstyle' />
+		<canvas id='submissions' />
 	</div>
-	<div class="charts-bar">
-		<canvas id="loc" />
+	<div class='charts-bar'>
+		<canvas id='loc' />
 	</div>
-	<div class="charts-referer">
-		<canvas id="referer" />
+	<div class='charts-referer'>
+		<canvas id='referer' />
 	</div>
 </main>
 
-<style lang="scss">
-	main {
-		display: flex;
+<style lang='scss'>
+  main {
+    display: flex;
 
-		flex-direction: column;
-		justify-content: center;
-	}
+    flex-direction: column;
+    justify-content: center;
+  }
 
-	h1,
-	h3,
-	a {
-		text-align: center;
-	}
+  h1,
+  h3,
+  a {
+    text-align: center;
+  }
 
-	a {
-		font-size: x-large;
-		padding: 1em;
-	}
+  a {
+    font-size: x-large;
+    padding: 1em;
+  }
 
-	.charts {
-		position: relative;
-		display: flex;
-		justify-content: flex-start;
+  .charts {
+    position: relative;
+    display: flex;
+    justify-content: flex-start;
 
-		height: 40%;
-		width: 50%;
-		//overflow:hidden;
-	}
+    height: 40%;
+    width: 50%;
+    //overflow:hidden;
+  }
 
-	.charts-bar,
-	.charts-referer {
-		height: 40%;
-	}
+  .charts-bar,
+  .charts-referer {
+    height: 40%;
+  }
 
-	canvas {
-		border: 0.15em solid $chromeblue;
-		padding: 1em;
-	}
+  canvas {
+    border: 0.15em solid $chromeblue;
+    padding: 1em;
+  }
 
-	.video-container {
-		align-self: center;
-	}
-	//.video-container {
-	//
-	//  position: relative;
-	//  padding-bottom: 56.25%;
-	//  padding-top: 30px;
-	//	max-width:80%;
-	//  height: 0;
-	//  overflow: hidden;
-	//
-	//  iframe,
-	//  object,
-	//  embed {
-	//    position: absolute;
-	//    top: 0;
-	//    left: 0;
-	//    width: 100%;
-	//    height: 100%;
-	//  }
-	//}
+  .video-container {
+
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 30px;
+    height: 0;
+    overflow: hidden;
+
+    iframe, object, embed {
+
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  @media screen and (max-width: 849px) {
+    main {
+      display: block;
+    }
+    canvas {
+      border: 0.1em solid $chromeblue;
+      padding: 0.1em;
+    }
+
+    .charts {
+      position: relative;
+      display: block;
+      column-count: 1;
+      height: 100%;
+      width: 100%;
+      //overflow:hidden;
+    }
+
+    .charts-bar,
+    .charts-referer {
+
+      position: relative;
+      display: block;
+      column-count: 1;
+      min-height: 400px;
+      min-width: 100%;
+      width: 100%;
+      height: 100%;
+
+      canvas {
+        height: 100%;
+
+      }
+    }
+
+    .video-container {
+      position: relative;
+      padding-bottom: 56.25%; /* 16:9 */
+      height: 0;
+
+      iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+  }
+
 </style>
