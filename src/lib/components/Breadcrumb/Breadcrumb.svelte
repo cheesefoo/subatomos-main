4a518e5bc24f18ca639f2d65af719c93fbe46bb0
cheesefoo <cheesefoo666@gmail.com>
2021-10-04 18:42:53 -0400

    wip

---
+++ src/lib/components/Breadcrumb/Breadcrumb.svelte
@@ -0,0 +1,26 @@
+<script>
+	/** @extends {"./BreadcrumbSkeleton"} BreadcrumbSkeletonProps */
+	/** Set to `true` to hide the breadcrumb trailing slash */
+	export let noTrailingSlash = false;
+	/** Set to `true` to display skeleton state */
+	export let skeleton = false;
+	import BreadcrumbSkeleton from './BreadcrumbSkeleton.svelte';
+</script>
+
+<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+{#if skeleton}
+	<BreadcrumbSkeleton
+		{noTrailingSlash}
+		{...$$restProps}
+		on:click
+		on:mouseover
+		on:mouseenter
+		on:mouseleave
+	/>
+{:else}
+	<nav aria-label="Breadcrumb" {...$$restProps} on:click on:mouseover on:mouseenter on:mouseleave>
+		<ol class:breadcrumb={true} class:breadcrumb-no-trailing-slash={noTrailingSlash}>
+			<slot />
+		</ol>
+	</nav>
+{/if}
