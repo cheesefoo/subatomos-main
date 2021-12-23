c315de8e1854347f5ff58cba8147c5402b878461
cheesefoo <cheesefoo666@gmail.com>
2021-12-23 15:06:26 -0500

    fix local links

--- src/lib/components/TooltipLoader.svelte
+++ src/lib/components/TooltipLoader.svelte
@@ -32,12 +32,12 @@
 	// export let slug, excerpt, text = undefined;
 </script>
 {#await getExcerpt()}
-	<a href="./{slug}">{text}</a>
+	<a href="../{slug}">{text}</a>
 	{:then res}
 	<!--{@debug res}-->
 	{#if res.props.excerpt.length < 480}
 	<a
-		href="./{slug}"
+		href="../{slug}"
 		class='excerpt'
 		use:tooltip={{ component: WikiTooltip, props: { hoverText: res.props.excerpt } }}>
 		{text}
@@ -45,7 +45,7 @@
 <!--		<WikiTooltip slug={slug} text={text} hoverText={res.props.excerpt}/>-->
 		{/if}
 	{:catch err}
-	<a href="./{slug}">{text}</a>
+	<a href="../{slug}">{text}</a>
 
 {/await}
 <style>
