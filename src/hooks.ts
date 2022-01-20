a3ba9a75838ab6c196d234721310bf2e2b36425d
cheesefoo <cheesefoo666@gmail.com>
2022-01-19 22:01:01 -0500

    update hooks.ts

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,7 +2,8 @@ import cookie from 'cookie';
 import { v4 as uuid } from '@lukeed/uuid';
 import type { Handle } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ request, resolve }) => {
+export async function  handle({ event, resolve })  {
+/*
 	const cookies = cookie.parse(request.headers.cookie || '');
 	request.locals.userid = cookies.userid || uuid();
 
@@ -10,14 +11,17 @@ export const handle: Handle = async ({ request, resolve }) => {
 	if (request.url.searchParams.has('_method')) {
 		request.method = request.url.searchParams.get('_method').toUpperCase();
 	}
+*/
 
-	const response = await resolve(request);
+	const response = await resolve(event);
 
+/*
 	if (!cookies.userid) {
 		// if this is the first time the user has visited this app,
 		// set a cookie so that we recognise them when they return
 		response.headers['set-cookie'] = `userid=${request.locals.userid}; Path=/; HttpOnly`;
 	}
+*/
 
 	return response;
 };
