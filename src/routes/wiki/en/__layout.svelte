d9bba30c70cced6aaf0b835148f79f2d1cc407f9
cheesefoo <cheesefoo666@gmail.com>
2022-02-10 19:22:54 -0500

    restyling

--- src/routes/wiki/en/__layout.svelte
+++ src/routes/wiki/en/__layout.svelte
@@ -48,4 +48,42 @@
 		background: url(/assets/images/sky.webp) no-repeat center center fixed;
 		background-size: cover;
 	}
+  .content {
+    padding: 2em 10% 0 4em;
+  }
+
+  h1, h3 {
+    color: darksalmon;
+    padding: 1em 0;
+  }
+
+  a {
+    color: $accent-color
+  }
+
+  h1 {
+    font-family: keifont, sans-serif;
+    font-size: 2em;
+    font-weight: bolder;
+  }
+
+  p,
+  a,
+  li,
+  ul {
+    font-family: keifont, sans-serif;
+    font-weight: 100;
+  }
+
+  h3 {
+    font-family: keifont, sans-serif;
+    font-size: 1.5em;
+  }
+
+  @media screen and (min-width: 849px) and (max-width: 1024px) {
+    .content {
+      padding: 0 1em 0 1em;
+    }
+
+  }
 </style>
