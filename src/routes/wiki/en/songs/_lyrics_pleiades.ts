40cee20a547c64d8a624c30a7492d5beab8cbe23
cheesefoo <cheesefoo666@gmail.com>
2022-05-02 23:11:29 -0400

    lyrics wip

---
+++ src/routes/wiki/en/songs/_lyrics_pleiades.ts
@@ -0,0 +1,128 @@
+export const jp = [['ちょこっとヤンチャで　ニコっと元気な',
+	'キラッとスマイル ア・イ・ド・ル！',
+	'毎日もっと　楽しくしたい',
+	'ドンヨク with アドベンチャー'],
+	[
+		'	ひらひらスカートも',
+		'ちゃんと着こなせるんだよ',
+		'だから もっともっと',
+		'好きになってもいいよ？',
+		'一緒に　大空に飛びたとう'
+	], [
+		'光れ Starlight　どんな輝きの中も',
+		'強いキラメキ',
+		'キミに届いているかな？',
+		'1ミリでも　伝われ Fight, oh!'
+	], [
+		'チャージ！チャレンジ！全力アクセル',
+		'がんばるキミへ',
+		'一番乗りのスマイルを贈る　スバル ch',
+		'Everyday Night&Day みんなで！'
+	], [
+		'ブルーな気持ちに　ズバッと駆けつけ',
+		'お助けしちゃう　ヒーロー！',
+		'弱いハートじゃ　いられないから',
+		'つよつよでいきましょう'
+	], [
+		'ハラハラなミズギで',
+		'ドキドキのトキメキ Time…？',
+		'いつでもおいでよ　一年中パラダイス',
+		'ドカンと曇り空吹き飛ばそう'
+	], [
+		'あがれ！スターマイン　どんな暗闇の中も',
+		'眩しく照らす',
+		'キミに見つけてもらえた',
+		'それだけでもう　運命なんだ'
+	], [
+		'もっとずーーっと道は続いてく',
+		'夢に向かって',
+		'一緒に来てくれるかな？',
+		'その手 とって　走れ',
+		'つれて いくよ　みんなで！'
+	], [
+		'ささやく (言葉は) 恥ずかしいけれど',
+		'(こっそり、ね) 伝えたい想い',
+		'今なら言える (あのね ドキドキ…)'
+	], [
+		'キミのエールがチカラになるよ',
+		'いつも元気を分けてくれて',
+		'頑張っていけるんだ　笑って過ごせるんだ',
+		'ホントに　ありがとうの気持ちを'
+	], [
+		'歌え！ Starlight　どんな輝きの中も',
+		'強いキラメキ',
+		'キミに届いているかな？',
+		'1ミリでも　伝われ Fight, oh!'
+	], [
+		'もっとずーーっと道は続いてく',
+		'夢に向かって',
+		'一緒に来てくれるかな？',
+		'その手　とって　走れ',
+		'つれていくよ　みんなで！',
+		'キミと　駆ける　スバル！'
+	]];
+
+export const romaji = [['Chokotto yancha de nikotto genkina',
+	'kiratto sumairu a i do ru!',
+	'Mainichi motto tanoshiku shitai',
+	'don’yoku u~izu adobencha'
+], [
+	'hirahira sukato mo',
+	'chanto kikonaseru nda yo',
+	'dakara motto motto',
+	'suki ni natte mo ii yo?',
+	'Issho ni ozora ni tobitatou'
+], [
+	'hikare sutaraito don’na kagayaki no naka mo',
+	'tsuyoi kirameki',
+	'kimi ni todoite iru ka na?',
+	'1-Miri demo tsutawa re Fight, oh!'
+], [
+	'Chaji! Charenji! Zenryoku akuseru',
+	'ganbaru kimi e',
+	'ichiban’nori no sumairu o okuru Subaru ch',
+	'eburidi Night& de min’nade!'
+], [
+	'Buruna kimochi ni zubatto kaketsuke',
+	'o tasuke shi chau hiro!',
+	'Yowai hato ja i rarenaikara',
+	'tsuyo tsuyode ikimashou'
+], [
+	'haraharana mizugi de',
+	'dokidoki no tokimeki ontaimu…?',
+	'Itsu demo oideyo ichinenju paradaisu',
+	'dokanto kumorizora fukitobasou'
+], [
+	'agare! Sutamain don’na kurayami no naka mo',
+	'mabushiku terasu',
+	'kimi ni mitsukete moraeta',
+	'sore dake de mo unmeina nda'
+], [
+	'mottozutto michi wa tsudzuite ku',
+	'yume ni mukatte',
+	'issho ni kite kureru ka na?',
+	'Sono te totte hashire',
+	'tsurete iku yo min’na de!'
+], [
+	'Sasayaku (kotoba wa) hazukashikeredo',
+	'(kossori, ne) tsutaetai omoi',
+	'imanara ieru (ano ne dokidoki…)'
+], [
+	'kimi no eru ga Chikara ni naru yo',
+	'itsumo genki o wakete kurete',
+	'ganbatte ikeru nda waratte sugoseru nda',
+	'honto ni arigato no kimochi o'
+], [
+	'utae! Sutaraito don’na kagayaki no naka mo',
+	'tsuyoi kirameki',
+	'kimi ni todoite iru ka na?',
+	'1-Miri demo tsutawa re Fight, oh!'
+], [
+	'Mottozutto michi wa tsudzuite ku',
+	'yume ni mukatte',
+	'issho ni kite kureru ka na?',
+	'Sono te totte hashire',
+	'tsurete iku yo min’na de!',
+	'Kimi to kakeru Subaru!']];
+
+export default { jp, romaji };
