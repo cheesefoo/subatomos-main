559361edc4df6f43b33b341cd3800d8d1956b341
cheesefoo <cheesefoo666@gmail.com>
2022-06-25 23:21:29 -0400

    toggle button, fix guitar

--- src/lib/map/MapToolbar.svelte
+++ src/lib/map/MapToolbar.svelte
@@ -30,6 +30,15 @@
 		{/if}
 	</svg>
 </button>-->
+<button type="button" class:selected={eye} on:click={() => dispatch('click-hide-map')} title="Hide Map">
+	<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="{eye ? 2 : 1}" viewBox="0 0 24 24" stroke="currentColor">
+		{#if eye}
+			<path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
+		{:else}
+			<path d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"></path>
+		{/if}
+	</svg>
+</button>
 
 <button type="button" on:click={clickLines} class:selected={lines} title="Show Lines">
 	<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="{lines ? 2 : 1}" viewBox="0 0 24 24" stroke="currentColor"><path d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path></svg>
